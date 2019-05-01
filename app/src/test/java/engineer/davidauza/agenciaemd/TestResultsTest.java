0849f59f1dddfcbec90680dd71d27aaa3d51ddda
Yeiner Fernandez <yeiner89@gmail.com>
2019-05-01 09:43:38 -0500

    Basic assertions on the Questions #1 and #2

--- app/src/test/java/engineer/davidauza/agenciaemd/TestResultsTest.java
+++ app/src/test/java/engineer/davidauza/agenciaemd/TestResultsTest.java
@@ -9,6 +9,17 @@ public class TestResultsTest {
     @Test
     public void score_is_correct() {
         TestResults tr = new TestResults();
+
+        // Assertions on Question #1
         assertEquals(3,  tr.getScore((byte) 0, true));
+        assertEquals(3,  tr.getScore((byte) 0, true, false, false, false));
+        assertEquals(6,  tr.getScore((byte) 0, true, true, false, false));
+        assertEquals(8,  tr.getScore((byte) 0, true, true, true, false));
+        assertEquals(5,  tr.getScore((byte) 0, true, false, true, false));
+        assertEquals(7,  tr.getScore((byte) 0, true, false, true, true));
+
+        // Assertions on Question #2
+        assertEquals(10,  tr.getScore((byte) 1, true));
+        assertEquals(0,  tr.getScore((byte) 1, false));
     }
 }
