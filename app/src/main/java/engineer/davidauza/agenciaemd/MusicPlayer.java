7964dc417a561cf00f45d113f9f456df949400d9
davidauza.engineer <davidauza01@gmail.com>
2019-05-01 11:18:21 -0500

    Music Player layout done.

---
+++ app/src/main/java/engineer/davidauza/agenciaemd/MusicPlayer.java
@@ -0,0 +1,13 @@
+package engineer.davidauza.agenciaemd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MusicPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
